ced5855235ccbdb84dba697e4e919beaadfb2b40
MadhuriVasamsetti22 <vmadhu2256@gmail.com>
2026-06-01 14:06:59 +0530

    Add deployment config

---
+++ setup.sh
@@ -0,0 +1,3 @@
+pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip install scispacy
+pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz
